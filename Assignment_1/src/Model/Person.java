e85e3ca151f3a1143d6d798af46e1d5252dc0eb3
VivekNikam21 <nikam.vi@northeastern.edu>
2024-10-06 21:41:52 -0400

    Assignment 1 reupload

---
+++ Assignment_1/src/Model/Person.java
@@ -0,0 +1,205 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author nikam
+ */
+public class Person {
+
+    String fname;
+    String lname;
+    String email;
+    String phone;
+    String dlnumber;
+    String dlstate;
+    String ssn;
+    String addl1;
+    String addl2;
+    String city;
+    String state;
+    String zip;
+    String telenumber;
+    String faxnumber;
+    String dob;
+    String height;
+    String weight;
+    String university;
+    String campus;
+    String course; 
+    String gradyear;
+    
+    public String getGradyear() {
+        return gradyear;
+    }
+
+    public void setGradyear(String gradyear) {
+        this.gradyear = gradyear;
+    }
+    
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDlnumber() {
+        return dlnumber;
+    }
+
+    public void setDlnumber(String dlnumber) {
+        this.dlnumber = dlnumber;
+    }
+
+    public String getDlstate() {
+        return dlstate;
+    }
+
+    public void setDlstate(String dlstate) {
+        this.dlstate = dlstate;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getAddl1() {
+        return addl1;
+    }
+
+    public void setAddl1(String addl1) {
+        this.addl1 = addl1;
+    }
+
+    public String getAddl2() {
+        return addl2;
+    }
+
+    public void setAddl2(String addl2) {
+        this.addl2 = addl2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelenumber() {
+        return telenumber;
+    }
+
+    public void setTelenumber(String telenumber) {
+        this.telenumber = telenumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    
+    
+}
